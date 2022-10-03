fd4c87fe00a15d05ad3780bcd04281037b2656d4
Victor Alonso <victor@ieselcaminas.org>
2022-10-03 12:18:42 +0200

    spaces

--- src/Example32.java
+++ src/Example32.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Example32 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double x,y ,n;
         Scanner input = new Scanner(System.in);
         System.out.println("enter a number: ");
@@ -10,7 +10,7 @@ public class Example32 {
 
         x = n;
         System.out.println(1/2);
-        for (int i =1; i <= 10; i++){
+        for (int i =1; i <= 10; i++) {
             y = 1.0 / 2.0* (x + n / x);
             x = y;
             System.out.println(x);
